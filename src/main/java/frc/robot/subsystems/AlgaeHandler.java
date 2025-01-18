9a018790ad3e40a336596fbeb91920472ce90d81
Gloria <mcalping@ccsdbraves.org>
2025-01-18 10:36:24 -0500

    Created Algae-Handler branch, I will be creating algae subsystem here :)

---
+++ src/main/java/frc/robot/subsystems/AlgaeHandler.java
@@ -0,0 +1,26 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.lib.subsystem.AdvancedSubsystem;
+
+
+public class AlgaeHandler extends AdvancedSubsystem {
+  /** Creates a new AlgaeHandler. */
+  public AlgaeHandler() {}
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+
+  @Override
+  protected Command systemCheckCommand() {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'systemCheckCommand'");
+  }
+}
