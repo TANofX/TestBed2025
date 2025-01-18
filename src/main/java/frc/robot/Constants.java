c54baded6351101bdc2a7b1da06e093b96e08990
Dreamgineer <superdinmc@gmail.com>
2025-01-18 09:53:30 -0500

    Import cleanup

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -11,7 +11,6 @@ import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.numbers.N1;
 import edu.wpi.first.math.numbers.N3;
 import edu.wpi.first.math.util.Units;
-import edu.wpi.first.wpilibj.XboxController;
 
 import java.io.IOException;
 
