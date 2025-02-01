798908d328e6587c414f7ecb1ab8a9b4cf93c8b0
BOBCARLOS <91758063+talbota@users.noreply.github.com>
2025-02-01 15:42:50 -0500

    Added the various oordinates of the different coral branches to help with autoaiming

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,12 +4,16 @@ import edu.wpi.first.apriltag.AprilTagFieldLayout;
 import edu.wpi.first.apriltag.AprilTagFields;
 import edu.wpi.first.math.Matrix;
 import edu.wpi.first.math.VecBuilder;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.numbers.N1;
 import edu.wpi.first.math.numbers.N3;
 import edu.wpi.first.math.util.Units;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public final class Constants {
   public static final String canivoreBusName = "rio";
@@ -72,8 +76,47 @@ public final class Constants {
       // public static final PIDConstants ROTATION_CONSTANTS = new PIDConstants(2.0,
       // 0.0, 0.0);
     }
+    
 
-    public static final class FrontLeftModule {
+    //Class to access the coordinates of the coral on the field.
+    public static final class CoralPlacement {
+      public static ArrayList<Pose2d> cordinatesCoralRed = new ArrayList<Pose2d>();
+      static {
+      //ordered in line from A-L
+      //rotation degree part of Pos2D is the direction the robot has to face to be flush against the reef for that branch
+      cordinatesCoralRed.add(new Pose2d(544.87, 152.03, Rotation2d.fromDegrees(180))); 
+      cordinatesCoralRed.add(new Pose2d(544.87, 164.97,  Rotation2d.fromDegrees(180))); 
+      cordinatesCoralRed.add(new Pose2d(535.08, 181.89,  Rotation2d.fromDegrees(240)));
+      cordinatesCoralRed.add(new Pose2d(523.90, 188.32,  Rotation2d.fromDegrees(240)));
+      cordinatesCoralRed.add(new Pose2d(504.39, 188.32,  Rotation2d.fromDegrees(300))); 
+      cordinatesCoralRed.add(new Pose2d(493.16, 181.89,  Rotation2d.fromDegrees(300)));
+      cordinatesCoralRed.add(new Pose2d(483.44, 164.97,  Rotation2d.fromDegrees(0.0))); 
+      cordinatesCoralRed.add(new Pose2d(483.44, 152.03,  Rotation2d.fromDegrees(0.0))); 
+      cordinatesCoralRed.add(new Pose2d(493.16, 135.15,  Rotation2d.fromDegrees(60))); 
+      cordinatesCoralRed.add(new Pose2d(504.39, 128.65,  Rotation2d.fromDegrees(60))); 
+      cordinatesCoralRed.add(new Pose2d(523.90, 128.65,  Rotation2d.fromDegrees(120))); 
+      cordinatesCoralRed.add(new Pose2d(535.08, 135.15,  Rotation2d.fromDegrees(120)));
+      }
+
+      public static ArrayList<Pose2d> cordinatesCoralBlue = new ArrayList<Pose2d>();
+      static {
+      //ordered in line from A-L, even though this is "opposite" of blue
+      cordinatesCoralBlue.add(new Pose2d(146.052, 164.97, Rotation2d.fromDegrees(0.0)));
+      cordinatesCoralBlue.add(new Pose2d(146.052, 152.03,  Rotation2d.fromDegrees(0.0))); 
+      cordinatesCoralBlue.add(new Pose2d(155.43, 135.15, Rotation2d.fromDegrees(60))); 
+      cordinatesCoralBlue.add(new Pose2d(166.65, 128.65, Rotation2d.fromDegrees(60))); 
+      cordinatesCoralBlue.add(new Pose2d(136.51, 128.65, Rotation2d.fromDegrees(120))); 
+      cordinatesCoralBlue.add(new Pose2d(197.69, 135.15, Rotation2d.fromDegrees(120))); 
+      cordinatesCoralBlue.add(new Pose2d(207.48, 152.03, Rotation2d.fromDegrees(180)));
+      cordinatesCoralBlue.add(new Pose2d(207.48, 164.97, Rotation2d.fromDegrees(180))); 
+      cordinatesCoralBlue.add(new Pose2d(197.69, 181.89, Rotation2d.fromDegrees(240))); 
+      cordinatesCoralBlue.add(new Pose2d(186.51, 188.32, Rotation2d.fromDegrees(240)));
+      cordinatesCoralBlue.add(new Pose2d(166.65, 188.32, Rotation2d.fromDegrees(300)));
+      cordinatesCoralBlue.add(new Pose2d(155.43, 181.89, Rotation2d.fromDegrees(300))); 
+      }
+      }
+      
+      public static final class FrontLeftModule {
       public static final int driveMotorCanID = 7;
       public static final int rotationMotorCanID = 8;
       public static final int rotationEncoderCanID = 13;
